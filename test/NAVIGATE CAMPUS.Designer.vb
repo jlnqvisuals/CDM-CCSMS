da7283284863570c48433127625af050c1b0e8ff
jlnq <jlnq@DESKTOP-TFASIDU>
2024-06-06 00:37:22 +0800

    Q.Update

--- test/NAVIGATE CAMPUS.Designer.vb
+++ test/NAVIGATE CAMPUS.Designer.vb
@@ -22,27 +22,25 @@ Partial Class NAVIGATE_CAMPUS
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(NAVIGATE_CAMPUS))
         adminBldgBtn = New Button()
         prrcBldgBtn = New Button()
         newBldgBtn = New Button()
         adminBldgPanel = New Panel()
+        Label1 = New Label()
+        Label2 = New Label()
         adminBackBtn = New Button()
         adminNavBtn = New Button()
-        Label3 = New Label()
-        Label2 = New Label()
-        Label1 = New Label()
         prrcBldgPanel = New Panel()
         prrcBackBtn = New Button()
         prrcNavBtn = New Button()
         Label7 = New Label()
         Label8 = New Label()
-        lale = New Label()
         newBldgPanel = New Panel()
-        newBackBtn = New Button()
-        newNavBtn = New Button()
         Label4 = New Label()
         Label5 = New Label()
-        Label6 = New Label()
+        newBackBtn = New Button()
+        newNavBtn = New Button()
         Button2 = New Button()
         adminBldgPanel.SuspendLayout()
         prrcBldgPanel.SuspendLayout()
@@ -51,241 +49,278 @@ Partial Class NAVIGATE_CAMPUS
         ' 
         ' adminBldgBtn
         ' 
-        adminBldgBtn.Location = New Point(195, 168)
+        adminBldgBtn.BackColor = Color.Transparent
+        adminBldgBtn.BackgroundImage = CType(resources.GetObject("adminBldgBtn.BackgroundImage"), Image)
+        adminBldgBtn.BackgroundImageLayout = ImageLayout.None
+        adminBldgBtn.Cursor = Cursors.Hand
+        adminBldgBtn.FlatAppearance.BorderSize = 0
+        adminBldgBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        adminBldgBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        adminBldgBtn.FlatStyle = FlatStyle.Flat
+        adminBldgBtn.ForeColor = SystemColors.ControlText
+        adminBldgBtn.Location = New Point(152, 144)
         adminBldgBtn.Name = "adminBldgBtn"
-        adminBldgBtn.Size = New Size(104, 35)
+        adminBldgBtn.Size = New Size(155, 125)
         adminBldgBtn.TabIndex = 0
-        adminBldgBtn.Text = "ADMIN BLDG"
-        adminBldgBtn.UseVisualStyleBackColor = True
+        adminBldgBtn.UseVisualStyleBackColor = False
         ' 
         ' prrcBldgBtn
         ' 
-        prrcBldgBtn.Location = New Point(219, 359)
+        prrcBldgBtn.BackColor = Color.Transparent
+        prrcBldgBtn.BackgroundImage = CType(resources.GetObject("prrcBldgBtn.BackgroundImage"), Image)
+        prrcBldgBtn.BackgroundImageLayout = ImageLayout.None
+        prrcBldgBtn.Cursor = Cursors.Hand
+        prrcBldgBtn.FlatAppearance.BorderSize = 0
+        prrcBldgBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        prrcBldgBtn.FlatStyle = FlatStyle.Flat
+        prrcBldgBtn.ForeColor = SystemColors.ControlText
+        prrcBldgBtn.Location = New Point(241, 358)
         prrcBldgBtn.Name = "prrcBldgBtn"
-        prrcBldgBtn.Size = New Size(94, 42)
+        prrcBldgBtn.Size = New Size(155, 125)
         prrcBldgBtn.TabIndex = 0
-        prrcBldgBtn.Text = "PRRC BLDG"
-        prrcBldgBtn.UseVisualStyleBackColor = True
+        prrcBldgBtn.UseVisualStyleBackColor = False
         ' 
         ' newBldgBtn
         ' 
-        newBldgBtn.Location = New Point(339, 242)
+        newBldgBtn.BackColor = Color.Transparent
+        newBldgBtn.BackgroundImage = CType(resources.GetObject("newBldgBtn.BackgroundImage"), Image)
+        newBldgBtn.BackgroundImageLayout = ImageLayout.None
+        newBldgBtn.Cursor = Cursors.Hand
+        newBldgBtn.FlatAppearance.BorderSize = 0
+        newBldgBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        newBldgBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        newBldgBtn.FlatStyle = FlatStyle.Flat
+        newBldgBtn.ForeColor = SystemColors.ControlText
+        newBldgBtn.Location = New Point(422, 217)
         newBldgBtn.Name = "newBldgBtn"
-        newBldgBtn.Size = New Size(104, 52)
+        newBldgBtn.Size = New Size(155, 125)
         newBldgBtn.TabIndex = 0
-        newBldgBtn.Text = "NEW BLDG"
-        newBldgBtn.UseVisualStyleBackColor = True
+        newBldgBtn.UseVisualStyleBackColor = False
         ' 
         ' adminBldgPanel
         ' 
+        adminBldgPanel.BackColor = Color.Transparent
+        adminBldgPanel.BackgroundImage = CType(resources.GetObject("adminBldgPanel.BackgroundImage"), Image)
+        adminBldgPanel.BackgroundImageLayout = ImageLayout.Stretch
+        adminBldgPanel.Controls.Add(Label1)
+        adminBldgPanel.Controls.Add(Label2)
         adminBldgPanel.Controls.Add(adminBackBtn)
         adminBldgPanel.Controls.Add(adminNavBtn)
-        adminBldgPanel.Controls.Add(Label3)
-        adminBldgPanel.Controls.Add(Label2)
-        adminBldgPanel.Controls.Add(Label1)
-        adminBldgPanel.Location = New Point(594, 57)
+        adminBldgPanel.Location = New Point(654, 78)
         adminBldgPanel.Name = "adminBldgPanel"
-        adminBldgPanel.Size = New Size(348, 463)
+        adminBldgPanel.Size = New Size(347, 459)
         adminBldgPanel.TabIndex = 1
-        ' 
-        ' adminBackBtn
-        ' 
-        adminBackBtn.Location = New Point(249, 425)
-        adminBackBtn.Name = "adminBackBtn"
-        adminBackBtn.Size = New Size(75, 23)
-        adminBackBtn.TabIndex = 1
-        adminBackBtn.Text = "Back"
-        adminBackBtn.UseVisualStyleBackColor = True
-        ' 
-        ' adminNavBtn
-        ' 
-        adminNavBtn.Location = New Point(15, 425)
-        adminNavBtn.Name = "adminNavBtn"
-        adminNavBtn.Size = New Size(75, 23)
-        adminNavBtn.TabIndex = 1
-        adminNavBtn.Text = "Navigate"
-        adminNavBtn.UseVisualStyleBackColor = True
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label3.Location = New Point(15, 162)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(240, 32)
-        Label3.TabIndex = 0
-        Label3.Text = "Total Room Count: ??"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label2.Location = New Point(15, 100)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(161, 32)
-        Label2.TabIndex = 0
-        Label2.Text = "Floor count: 2"
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(15, 12)
+        Label1.Font = New Font("Arial", 15F)
+        Label1.Location = New Point(30, 307)
         Label1.Name = "Label1"
-        Label1.Size = New Size(157, 32)
-        Label1.TabIndex = 0
-        Label1.Text = "ADMIN BLDG"
+        Label1.Size = New Size(201, 23)
+        Label1.TabIndex = 6
+        Label1.Text = "Total Room Count: ??"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 15F)
+        Label2.Location = New Point(30, 280)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(131, 23)
+        Label2.TabIndex = 7
+        Label2.Text = "Floor count: 4"
+        ' 
+        ' adminBackBtn
+        ' 
+        adminBackBtn.Cursor = Cursors.Hand
+        adminBackBtn.FlatAppearance.BorderSize = 0
+        adminBackBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        adminBackBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        adminBackBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        adminBackBtn.FlatStyle = FlatStyle.Flat
+        adminBackBtn.Location = New Point(188, 410)
+        adminBackBtn.Name = "adminBackBtn"
+        adminBackBtn.Size = New Size(75, 23)
+        adminBackBtn.TabIndex = 1
+        adminBackBtn.UseVisualStyleBackColor = True
+        ' 
+        ' adminNavBtn
+        ' 
+        adminNavBtn.Cursor = Cursors.Hand
+        adminNavBtn.FlatAppearance.BorderSize = 0
+        adminNavBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        adminNavBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        adminNavBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        adminNavBtn.FlatStyle = FlatStyle.Flat
+        adminNavBtn.Location = New Point(84, 410)
+        adminNavBtn.Name = "adminNavBtn"
+        adminNavBtn.Size = New Size(75, 23)
+        adminNavBtn.TabIndex = 1
+        adminNavBtn.UseVisualStyleBackColor = True
         ' 
         ' prrcBldgPanel
         ' 
+        prrcBldgPanel.BackColor = Color.Transparent
+        prrcBldgPanel.BackgroundImage = CType(resources.GetObject("prrcBldgPanel.BackgroundImage"), Image)
+        prrcBldgPanel.BackgroundImageLayout = ImageLayout.Stretch
         prrcBldgPanel.Controls.Add(prrcBackBtn)
         prrcBldgPanel.Controls.Add(prrcNavBtn)
         prrcBldgPanel.Controls.Add(Label7)
         prrcBldgPanel.Controls.Add(Label8)
-        prrcBldgPanel.Controls.Add(lale)
-        prrcBldgPanel.Location = New Point(592, 57)
+        prrcBldgPanel.Location = New Point(654, 78)
         prrcBldgPanel.Name = "prrcBldgPanel"
-        prrcBldgPanel.Size = New Size(348, 463)
+        prrcBldgPanel.Size = New Size(347, 459)
         prrcBldgPanel.TabIndex = 1
         ' 
         ' prrcBackBtn
         ' 
-        prrcBackBtn.Location = New Point(249, 425)
+        prrcBackBtn.Cursor = Cursors.Hand
+        prrcBackBtn.FlatAppearance.BorderSize = 0
+        prrcBackBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        prrcBackBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        prrcBackBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        prrcBackBtn.FlatStyle = FlatStyle.Flat
+        prrcBackBtn.ForeColor = Color.Transparent
+        prrcBackBtn.Location = New Point(188, 410)
         prrcBackBtn.Name = "prrcBackBtn"
         prrcBackBtn.Size = New Size(75, 23)
         prrcBackBtn.TabIndex = 1
-        prrcBackBtn.Text = "Back"
         prrcBackBtn.UseVisualStyleBackColor = True
         ' 
         ' prrcNavBtn
         ' 
-        prrcNavBtn.Location = New Point(15, 425)
+        prrcNavBtn.Cursor = Cursors.Hand
+        prrcNavBtn.FlatAppearance.BorderSize = 0
+        prrcNavBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        prrcNavBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        prrcNavBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        prrcNavBtn.FlatStyle = FlatStyle.Flat
+        prrcNavBtn.ForeColor = Color.Transparent
+        prrcNavBtn.Location = New Point(83, 410)
         prrcNavBtn.Name = "prrcNavBtn"
         prrcNavBtn.Size = New Size(75, 23)
         prrcNavBtn.TabIndex = 1
-        prrcNavBtn.Text = "Navigate"
         prrcNavBtn.UseVisualStyleBackColor = True
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
-        Label7.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label7.Location = New Point(15, 162)
+        Label7.Font = New Font("Arial", 15F)
+        Label7.Location = New Point(30, 307)
         Label7.Name = "Label7"
-        Label7.Size = New Size(240, 32)
+        Label7.Size = New Size(201, 23)
         Label7.TabIndex = 0
         Label7.Text = "Total Room Count: ??"
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label8.Location = New Point(15, 100)
+        Label8.Font = New Font("Arial", 15F)
+        Label8.Location = New Point(30, 280)
         Label8.Name = "Label8"
-        Label8.Size = New Size(161, 32)
+        Label8.Size = New Size(131, 23)
         Label8.TabIndex = 0
         Label8.Text = "Floor count: 4"
         ' 
-        ' lale
-        ' 
-        lale.AutoSize = True
-        lale.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        lale.Location = New Point(15, 12)
-        lale.Name = "lale"
-        lale.Size = New Size(135, 32)
-        lale.TabIndex = 0
-        lale.Text = "PRRC BLDG"
-        ' 
         ' newBldgPanel
         ' 
-        newBldgPanel.Controls.Add(newBackBtn)
-        newBldgPanel.Controls.Add(newNavBtn)
+        newBldgPanel.BackColor = Color.Transparent
+        newBldgPanel.BackgroundImage = CType(resources.GetObject("newBldgPanel.BackgroundImage"), Image)
+        newBldgPanel.BackgroundImageLayout = ImageLayout.Stretch
         newBldgPanel.Controls.Add(Label4)
         newBldgPanel.Controls.Add(Label5)
-        newBldgPanel.Controls.Add(Label6)
-        newBldgPanel.Location = New Point(592, 57)
+        newBldgPanel.Controls.Add(newBackBtn)
+        newBldgPanel.Controls.Add(newNavBtn)
+        newBldgPanel.Location = New Point(654, 78)
         newBldgPanel.Name = "newBldgPanel"
-        newBldgPanel.Size = New Size(348, 463)
+        newBldgPanel.Size = New Size(347, 459)
         newBldgPanel.TabIndex = 1
-        ' 
-        ' newBackBtn
-        ' 
-        newBackBtn.Location = New Point(249, 425)
-        newBackBtn.Name = "newBackBtn"
-        newBackBtn.Size = New Size(75, 23)
-        newBackBtn.TabIndex = 1
-        newBackBtn.Text = "Back"
-        newBackBtn.UseVisualStyleBackColor = True
-        ' 
-        ' newNavBtn
-        ' 
-        newNavBtn.Location = New Point(15, 425)
-        newNavBtn.Name = "newNavBtn"
-        newNavBtn.Size = New Size(75, 23)
-        newNavBtn.TabIndex = 1
-        newNavBtn.Text = "Navigate"
-        newNavBtn.UseVisualStyleBackColor = True
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label4.Location = New Point(15, 162)
+        Label4.Font = New Font("Arial", 15F)
+        Label4.Location = New Point(30, 307)
         Label4.Name = "Label4"
-        Label4.Size = New Size(240, 32)
-        Label4.TabIndex = 0
+        Label4.Size = New Size(201, 23)
+        Label4.TabIndex = 4
         Label4.Text = "Total Room Count: ??"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label5.Location = New Point(15, 100)
+        Label5.Font = New Font("Arial", 15F)
+        Label5.Location = New Point(30, 280)
         Label5.Name = "Label5"
-        Label5.Size = New Size(161, 32)
-        Label5.TabIndex = 0
+        Label5.Size = New Size(131, 23)
+        Label5.TabIndex = 5
         Label5.Text = "Floor count: 4"
         ' 
-        ' Label6
+        ' newBackBtn
         ' 
-        Label6.AutoSize = True
-        Label6.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label6.Location = New Point(15, 12)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(131, 32)
-        Label6.TabIndex = 0
-        Label6.Text = "NEW BLDG"
+        newBackBtn.BackgroundImageLayout = ImageLayout.None
+        newBackBtn.Cursor = Cursors.Hand
+        newBackBtn.FlatAppearance.BorderSize = 0
+        newBackBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        newBackBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        newBackBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        newBackBtn.FlatStyle = FlatStyle.Flat
+        newBackBtn.Location = New Point(188, 410)
+        newBackBtn.Name = "newBackBtn"
+        newBackBtn.Size = New Size(75, 23)
+        newBackBtn.TabIndex = 1
+        newBackBtn.UseVisualStyleBackColor = True
+        ' 
+        ' newNavBtn
+        ' 
+        newNavBtn.BackgroundImageLayout = ImageLayout.None
+        newNavBtn.Cursor = Cursors.Hand
+        newNavBtn.FlatAppearance.BorderSize = 0
+        newNavBtn.FlatAppearance.CheckedBackColor = Color.Transparent
+        newNavBtn.FlatAppearance.MouseDownBackColor = Color.Transparent
+        newNavBtn.FlatAppearance.MouseOverBackColor = Color.Transparent
+        newNavBtn.FlatStyle = FlatStyle.Flat
+        newNavBtn.Location = New Point(83, 410)
+        newNavBtn.Name = "newNavBtn"
+        newNavBtn.Size = New Size(75, 23)
+        newNavBtn.TabIndex = 1
+        newNavBtn.UseVisualStyleBackColor = True
         ' 
         ' Button2
         ' 
         Button2.BackColor = Color.Transparent
         Button2.Cursor = Cursors.Hand
+        Button2.FlatAppearance.BorderColor = Color.FromArgb(CByte(156), CByte(156), CByte(156))
         Button2.FlatAppearance.BorderSize = 0
         Button2.FlatAppearance.MouseDownBackColor = Color.Transparent
         Button2.FlatAppearance.MouseOverBackColor = Color.Transparent
         Button2.FlatStyle = FlatStyle.Flat
         Button2.ForeColor = Color.Black
-        Button2.Location = New Point(909, 9)
+        Button2.Location = New Point(1020, 12)
         Button2.Name = "Button2"
-        Button2.Size = New Size(70, 27)
+        Button2.Size = New Size(17, 17)
         Button2.TabIndex = 6
-        Button2.Text = "exit"
         Button2.UseVisualStyleBackColor = False
         ' 
         ' NAVIGATE_CAMPUS
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(990, 558)
+        BackColor = Color.White
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1050, 591)
         Controls.Add(Button2)
-        Controls.Add(adminBldgPanel)
         Controls.Add(newBldgBtn)
         Controls.Add(prrcBldgBtn)
         Controls.Add(adminBldgBtn)
-        Controls.Add(prrcBldgPanel)
         Controls.Add(newBldgPanel)
+        Controls.Add(prrcBldgPanel)
+        Controls.Add(adminBldgPanel)
+        ForeColor = SystemColors.ControlText
         FormBorderStyle = FormBorderStyle.None
         Name = "NAVIGATE_CAMPUS"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "NAVIGATE_CAMPUS"
         adminBldgPanel.ResumeLayout(False)
         adminBldgPanel.PerformLayout()
@@ -300,22 +335,19 @@ Partial Class NAVIGATE_CAMPUS
     Friend WithEvents prrcBldgBtn As Button
     Friend WithEvents newBldgBtn As Button
     Friend WithEvents adminBldgPanel As Panel
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
     Friend WithEvents adminBackBtn As Button
     Friend WithEvents adminNavBtn As Button
     Friend WithEvents newBldgPanel As Panel
     Friend WithEvents newBackBtn As Button
     Friend WithEvents newNavBtn As Button
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
     Friend WithEvents prrcBldgPanel As Panel
     Friend WithEvents prrcBackBtn As Button
     Friend WithEvents prrcNavBtn As Button
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
-    Friend WithEvents lale As Label
     Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- test/NAVIGATE CAMPUS.vb
+++ test/NAVIGATE CAMPUS.vb
@@ -1,4 +1,75 @@
-﻿Public Class NAVIGATE_CAMPUS
+﻿Imports System.Drawing.Drawing2D
+
+Public Class NAVIGATE_CAMPUS
+
+
+
+
+
+
+    Private Sub RoundedCorners(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        ' Create a rounded rectangle shape
+        Dim path As New GraphicsPath()
+        Dim rect As New Rectangle(0, 0, Me.Width, Me.Height)
+        Dim radius As Integer = 20 ' Adjust the radius to change the roundness of the corners
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
+        path.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90)
+        path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90)
+        path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+
+        ' Apply the custom region to the form
+        Me.Region = New Region(path)
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+    Private isDragging As Boolean = False
+    Private startPoint As Point
+
+    ' Event handler for Mouse Down event
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        If e.Button = MouseButtons.Left Then
+            isDragging = True
+            startPoint = New Point(e.X, e.Y)
+        End If
+    End Sub
+
+    ' Event handler for Mouse Move event
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If isDragging Then
+            Dim p As Point = PointToScreen(e.Location)
+            Location = New Point(p.X - startPoint.X, p.Y - startPoint.Y)
+        End If
+    End Sub
+
+    ' Event handler for Mouse Up event
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        If e.Button = MouseButtons.Left Then
+            isDragging = False
+        End If
+    End Sub
+
+
+
+
+
+
+
+
+
+
 
     Private Sub NAVIGATE_CAMPUS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         adminBldgPanel.Hide()
@@ -53,6 +124,14 @@
         Me.Close()
         MainDB_User.Show()
 
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
 
     End Sub
 End Class
